667d47fdb23a9793c97cf17e990381ab58df0fcd
Alejandro Ramirez Aldariz <627538+janodev@users.noreply.github.com>
2022-04-03 14:21:23 +0200

    Adds codable bool or string

---
+++ sources/main/CodableBoolOrString.swift
@@ -0,0 +1,55 @@
+import Foundation
+
+/**
+ Codable that represents an Bool or a String.
+
+ CodableBoolOrString solves cases where a JSON int appears as an Bool or a String:
+ ```
+ [
+     { "paywall_access": true },
+     { "paywall_access": "disabled" }
+ ]
+ ```
+
+ This JSON can be parsed as follows:
+ ```
+ struct Blog: Codable {
+     let paywallAccess: CodableBoolOrString
+ }
+
+ let jsonData = JSON.data(using: .utf8)!
+ let person = try! JSONDecoder().decode([Blog].self, from: jsonData)
+ ```
+ */
+public enum CodableBoolOrString: Codable, Hashable
+{
+    case bool(Bool)
+    case string(String)
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if let int: CodableBoolOrString = try? .bool(container.decode(Bool.self)) {
+            self = int
+        } else if let string: CodableBoolOrString = try? .string(container.decode(String.self)) {
+            self = string
+        } else {
+            throw DecodingError.typeMismatch(
+                CodableBoolOrString.self,
+                DecodingError.Context(
+                    codingPath: decoder.codingPath,
+                    debugDescription: "Expected a Bool or String"
+                )
+            )
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        switch self {
+        case .bool(let bool):
+            try container.encode(bool)
+        case .string(let string):
+            try container.encode(string)
+        }
+    }
+}
---
+++ sources/tests/model/CodableBoolOrStringTests.swift
@@ -0,0 +1,67 @@
+@testable import CodableHelpers
+import XCTest
+
+private struct Blog: Codable {
+    let paywallAccess: CodableBoolOrString
+
+    enum CodingKeys: String, CodingKey {
+        case paywallAccess = "paywall_access"
+    }
+}
+/**
+ Checks that Blog.age can be encoded/decoded between JSON and String or Bool.
+ ```
+ [
+     { "paywall_access": true },
+     { "paywall_access": "disabled" }
+ ]
+ ```
+ This JSON can be parsed as follows:
+ ```
+ struct Blog: Codable {
+     let paywallAccess: CodableBoolOrString
+ }
+
+ let jsonData = JSON.data(using: .utf8)!
+ let person = try! JSONDecoder().decode([Blog].self, from: jsonData)
+ ```
+ */
+final class CodableBoolOrStringTests: XCTestCase
+{
+    private let paywallStringJSON = "{\"paywall_access\":\"disabled\"}"
+    private let paywallBoolJSON = "{\"paywall_access\":true}"
+
+    /// Encode to `{ "paywall_access": true }`.
+    func testEncodeBool() throws
+    {
+        let blog = Blog(paywallAccess: CodableBoolOrString.bool(true))
+        let blogData: Data = try! JSONEncoder().encode(blog)
+        let JSON = String(data: blogData, encoding: String.Encoding.utf8)!
+        XCTAssertEqual(JSON, paywallBoolJSON)
+    }
+    
+    /// Encode to `{ "paywall_access": "disabled" }`.
+    func testEncodeString() throws
+    {
+        let blog = Blog(paywallAccess: CodableBoolOrString.string("disabled"))
+        let blogData: Data = try! JSONEncoder().encode(blog)
+        let JSON = String(data: blogData, encoding: String.Encoding.utf8)!
+        XCTAssertEqual(JSON, paywallStringJSON)
+    }
+
+    /// Decode from `{ "paywall_access": true }`.
+    func testDecodeFromJsonBool() throws
+    {
+        let blogData = paywallBoolJSON.data(using: .utf8)!
+        let blog = try! JSONDecoder().decode(Blog.self, from: blogData)
+        XCTAssertEqual(blog.paywallAccess, CodableBoolOrString.bool(true))
+    }
+
+    /// Decode from `{ "paywall_access": "disable" }`.
+    func testDecodeFromJsonString() throws
+    {
+        let blogData = paywallStringJSON.data(using: .utf8)!
+        let blog = try! JSONDecoder().decode(Blog.self, from: blogData)
+        XCTAssertEqual(blog.paywallAccess, CodableBoolOrString.string("disabled"))
+    }
+}
